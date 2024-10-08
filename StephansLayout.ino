42635b27e7f4d22bf6e5cf1441f95a92a2456622
Stephan Bösebeck <sb@caluga.de>
2024-10-08 09:13:12 +0200

    experiments

--- StephansLayout.ino
+++ StephansLayout.ino
@@ -224,7 +224,7 @@ KEYMAPS(
    Key_LeftGui, Key_Backspace, ShiftToLayer(SPECIAL), Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   Key_NonUsBackslashAndPipe,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_PcApplication,
+   Key_NonUsBackslashAndPipe,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Delete,
    Key_Enter,                  Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                                Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, MT(RightControl,Quote),
    Key_Hyper,                  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     MT(RightAlt,Minus),
