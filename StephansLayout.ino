2064c8994e115dc49a899b6ee22c0aba7ed4eb69
Stephan Bösebeck <sb@caluga.de>
2024-11-13 21:35:52 +0100

    homerowmod

--- StephansLayout.ino
+++ StephansLayout.ino
@@ -748,7 +748,7 @@ void setup() {
     QUKEYS(
         kaleidoscope::plugin::Qukey(0, KeyAddr(0, 6), ShiftToLayer(NUMPAD)),
          // left-side modifiers
-        kaleidoscope::plugin::Qukey(0, KeyAddr(3, 1), Key_LeftGui),      // A
+        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 1), Key_LeftGui),      // A
         kaleidoscope::plugin::Qukey(0, KeyAddr(2, 2), Key_LeftAlt),      // S
         kaleidoscope::plugin::Qukey(0, KeyAddr(2, 3), Key_LeftControl),  // D
         kaleidoscope::plugin::Qukey(0, KeyAddr(2, 4), Key_LeftShift),    // F
@@ -756,10 +756,10 @@ void setup() {
         kaleidoscope::plugin::Qukey(0, KeyAddr(3, 3), ShiftToLayer(SPECIAL)),    // C
         kaleidoscope::plugin::Qukey(0, KeyAddr(3, 4), ShiftToLayer(NUMPAD)),  // V
          // right-side modifiers
-        kaleidoscope::plugin::Qukey(0, KeyAddr(3, 14), Key_LeftGui),      // Ö
-        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 13), Key_LeftAlt),      // L
-        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 12), Key_LeftControl),  // K
-        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 11), Key_LeftShift),    // J
+        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 14), Key_RightGui),      // Ö
+        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 13), Key_RightAlt),      // L
+        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 12), Key_RightControl),  // K
+        kaleidoscope::plugin::Qukey(0, KeyAddr(2, 11), Key_RightShift),    // J
         // left-side layer shifts
         kaleidoscope::plugin::Qukey(0, KeyAddr(3, 12), ShiftToLayer(SPECIAL)),    // C
         //kaleidoscope::plugin::Qukey(0, KeyAddr(3, 11), ShiftToLayer(FUNCTION)),  // V
