7efaf96fbda3b4c7f607e45ddf5fd033c141b9d1
Stephan Bösebeck <sb@caluga.de>
2024-11-14 22:00:31 +0100

    reverting homerowmod

--- StephansLayout.ino
+++ StephansLayout.ino
@@ -204,15 +204,15 @@ KEYMAPS(
  [PRIMARY] = KEYMAP_STACKED
   (Key_Escape,      Key_1,         Key_2,         Key_3,           Key_4,             Key_5, LockLayer(NUMPAD),//M(LED_EFFECT_NEXT_NUMPADSHIFT),
    Key_Tab,         Key_Q,         Key_W,         Key_E,           Key_R,             Key_T, Key_LeftGui,
-   Key_LeftControl, MT(LeftAlt,A), MT(LeftGui,S), MT(LeftShift,D), MT(LeftControl,F), Key_G,
-   Key_LeftShift,   Key_Z,         Key_X,         LT(SPECIAL,C),   LT(NUMPAD,V),      Key_B, Key_Meh,
+   Key_LeftControl, Key_A,         Key_S,         Key_D,           Key_F,             Key_G,
+   Key_LeftShift,   Key_Z,         Key_X,         Key_C,           LT(NUMPAD,V),      Key_B, Key_Meh,
       Key_Backspace,   Key_Delete, Key_LeftAlt, ShiftToLayer(SPECIAL),
       ShiftToLayer(FUNCTION),
 
    M(LED_EFFECT_NEXT_NUMPADSHIFT), Key_6,        Key_7,               Key_8,             Key_9,            Key_0,                  Key_Minus,
    Key_RightGui,                   Key_Y,        Key_U,               Key_I,             Key_O,            Key_P,                  Key_LeftBracket,
-                                   Key_H,        MT(RightControl,J),  MT(RightShift,K),  MT(RightGui,L), MT(RightAlt,Semicolon), Key_Quote,
-   Key_Hyper,                      Key_N,        Key_M,               LT(SPECIAL,Comma), Key_Period,    Key_Slash,       Key_RightShift,
+                                   Key_H,        Key_J,               Key_K,             Key_L,            Key_Semicolon,          MT(RightControl,Quote),
+   Key_Hyper,                      Key_N,        Key_M,               Key_Comma,         Key_Period,       Key_Slash,              Key_RightShift,
       ShiftToLayer(SPECIAL),      Key_RightAlt, Key_Enter,  Key_Space,
       ShiftToLayer(FUNCTION)),
 
